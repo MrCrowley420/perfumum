778d7a6457ef3acc2c384d14fe6747b738690320
MrCrowley420 <monocorrea07@gmail.com>
2021-11-04 11:48:31 -0300

    agregando api del dolar

---
+++ js/app.js
@@ -0,0 +1,33 @@
+const url = "https://dolarsi.com/api/api.php?type=valoresprincipales"
+
+$("#botonDolar").click(() => {
+    $.get(url, (data, estado) => {
+        if (estado == "success") {
+            //  console.log(data)
+            console.log("DOLAR ACTUALIZADO")
+
+            data.forEach(element => {
+
+                $("#dolar").append(
+                    `
+                    <div class="card m-3
+                    " style="width: 18rem;">
+                        <img src="./imagenesLogos/dolar.gif" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title"></h5>
+                          <p class="card-text text-center">${element.casa.nombre}</p>
+                          <p class="card-text text-center text-danger">${element.casa.compra}</p>
+                          <p class="card-text text-center text-success">${element.casa.venta}</p>
+                          <div class="d-grid col-12 mx-auto">
+                          </div>
+                        </div>
+                    </div>
+                    `
+                );
+            })
+        }
+    });
+
+
+
+});
